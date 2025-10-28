a0eb2a3f8697af0439fbb078e154debbde57e98d
Deivi171 <158236668+Deivi171@users.noreply.github.com>
2025-10-27 20:21:10 -0700

    Avance de UI

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { HiMenuAlt4 } from 'react-icons/hi';
 import { AiOutlineClose } from 'react-icons/ai';
 
@@ -14,7 +14,7 @@ const NavbarItem = ({ title, classProps }) => {
 }
 
 const Navbar = () => {
-    const [toggleMenu, setToggleMenu] = React.useState(false);
+    const [toggleMenu, setToggleMenu] = useState(false);
     return (
         <nav className='w-full flex md:justify-center justify-between items-center p-4'>
             <div className='md:flex-[0.5] flex-initial justify-center items-center'>
@@ -34,7 +34,7 @@ const Navbar = () => {
                     : <HiMenuAlt4 fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(true)} />}
                 {toggleMenu && (
                     <ul
-                        className='z-10 fixed -top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
+                        className='z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
                     flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in'
                     >
                         <li className='text-xl w-full my-2'>
